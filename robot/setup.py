065aea7163a6c5f1645a1c55460067923e811d16
Charansc590 <charanvssc@gmail.com>
2025-08-07 16:02:11 +0530

    joystick code for real robot corrected and tested along with rpi code

---
+++ robot/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+package_name = 'robot'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='charansc',
+    maintainer_email='charanvssc@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+
+            'joy_sub = robot.joy_sub:main',
+            'hw_pub = robot.hw_pub:main',
+        ],
+    },
+)
